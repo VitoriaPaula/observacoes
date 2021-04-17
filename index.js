1e17e1b8ee3c5d1f0bb983a778d5c6f0f594ec73
Vitoria Paula <vitoriapaulalinsnunes@gmail.com>
2021-04-16 21:44:25 -0300

    atualizacao index

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ app.use(express.json());
 const axios = require('axios');
 
 const observacoesPorLembreteId = {};
+
+const funcoes = {
+    ObservacaoClassificada: (observacao) => {
+        const observacoes =
+            observacoesPorLembreteId[observacao.lembreteId];
+        const obsParaAtualizar = observacoes.find(o =>
+            o.id === observacao.id)
+        obsParaAtualizar.status = observacao.status;
+        axios.post("http://localhost:10000/eventos", {
+            tipo: "ObservacaoAtualizada",
+            dados: {
+                id: observacao.id,
+                texto: observacao.texto,
+                lembreteId: observacao.lembreteId,
+                status: observacao.status
+            }
+        });
+    }
+}
+
+
 const {
     v4: uuidv4
 } = require('uuid');
@@ -20,7 +41,8 @@ app.put('/lembretes/:id/observacoes', async (req, res) => {
         observacoesPorLembreteId[req.params.id] || [];
     observacoesDoLembrete.push({
         id: idObs,
-        texto
+        texto,
+        status: 'aguardando'
     });
     observacoesPorLembreteId[req.params.id] =
         observacoesDoLembrete;
@@ -29,7 +51,8 @@ app.put('/lembretes/:id/observacoes', async (req, res) => {
         dados: {
             id: idObs,
             texto,
-            lembreteId: req.params.id
+            lembreteId: req.params.id,
+            status: 'aguardando'
         }
     })
     res.status(201).send(observacoesDoLembrete);
@@ -38,6 +61,13 @@ app.get('/lembretes/:id/observacoes', (req, res) => {
     res.send(observacoesPorLembreteId[req.params.id] || []);
 
 });
+
+app.post('/eventos', (req, res) => {
+    try {
+        funcoes[req.body.tipo](req.body.dados);
+    } catch (err) { }
+    res.status(200).send({ msg: "ok" });
+})
 app.listen(5000, (() => {
     console.log('Lembretes  . Porta 5000');
 }));
